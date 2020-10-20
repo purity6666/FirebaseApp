33c58a1686f9f1305b83e4e55b315137c53cd695
purity6666 <68863394+purity6666@users.noreply.github.com>
2020-10-20 23:04:31 +0200

    Add files via upload

--- main/java/com/techta/firebaseapp1/EditUserActivity.java
+++ main/java/com/techta/firebaseapp1/EditUserActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -70,47 +71,75 @@ public class EditUserActivity extends AppCompatActivity {
         address = getIntent().getStringExtra("address");
 
         nameET = findViewById(R.id.nameET);
+        nameET.setText(name);
         ageET = findViewById(R.id.ageET);
+        ageET.setText(age);
         addressET = findViewById(R.id.addressET);
+        addressET.setText(address);
         zipET = findViewById(R.id.zipET);
+        zipET.setText(zip);
         updateUser = findViewById(R.id.updateButton);
 
         updateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserModel userModel = new UserModel();
-                userModel.setName(nameET.getText().toString());
-                userModel.setAge(Integer.parseInt(ageET.getText().toString()));
-                userModel.setZip_code(Long.parseLong(zipET.getText().toString()));
-                userModel.setAddress(addressET.getText().toString());
 
-                new FirebaseDatabaseHelper().updateUser(key, userModel, new FirebaseDatabaseHelper.DataStatus() {
-                    @Override
-                    public void DataIsLoaded(List<UserModel> userModels, List<String> keys) {
+                if (isEmpty()) {
+                    Toast.makeText(EditUserActivity.this, "Fields can't be left empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    UserModel userModel = new UserModel();
+                    userModel.setName(nameET.getText().toString());
+                    userModel.setAge(Integer.parseInt(ageET.getText().toString()));
+                    userModel.setZip_code(Long.parseLong(zipET.getText().toString()));
+                    userModel.setAddress(addressET.getText().toString());
 
-                    }
+                    new FirebaseDatabaseHelper().updateUser(key, userModel, new FirebaseDatabaseHelper.DataStatus() {
+                        @Override
+                        public void DataIsLoaded(List<UserModel> userModels, List<String> keys) {
 
-                    @Override
-                    public void DataIsInserted() {
+                        }
 
-                    }
+                        @Override
+                        public void DataIsInserted() {
 
-                    @Override
-                    public void DataIsUpdated() {
-                        Toast.makeText(EditUserActivity.this, "Successfully Updated", Toast.LENGTH_SHORT).show();
-                    }
+                        }
 
-                    @Override
-                    public void DataIsDeleted() {
+                        @Override
+                        public void DataIsUpdated() {
+                            Toast.makeText(EditUserActivity.this, "Successfully Updated", Toast.LENGTH_SHORT).show();
+                        }
 
-                    }
-                });
+                        @Override
+                        public void DataIsDeleted() {
 
-                finish();
-                return;
+                        }
+                    });
+
+                    finish();
+                    return;
+                }
             }
         });
 
+    }
 
+    private boolean isEmpty() {
+        if (TextUtils.isEmpty(nameET.getText().toString())) {
+            nameET.setError("Required!");
+            return true;
+        }
+        if (TextUtils.isEmpty(ageET.getText().toString())){
+            ageET.setError("Required!");
+            return true;
+        }
+        if (TextUtils.isEmpty(zipET.getText().toString())){
+            zipET.setError("Required!");
+            return true;
+        }
+        if (TextUtils.isEmpty(addressET.getText().toString())){
+            addressET.setError("Required!");
+            return true;
+        }
+        return false;
     }
 }
